558b924b45e276d1d2c62416c37c547560d618e0
shonmikl <mikhailegorov676@gmail.com>
2021-07-28 18:43:42 +0300

    added JUNIT

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -8,13 +8,13 @@ import java.util.Arrays;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws  NoSuchMethodException {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
-//        System.out.println(field.getName());
-//        System.out.println(field.get(r));
-//        field.set(r, "new_uuid");
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
 
         // TODO : invoke r.toString via reflection
         Method[] findToString = r.getClass().getMethods();
