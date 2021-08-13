0d8139c4db74ffa90ea39c32de3f72392e9479bb
shonmikl <mikhailegorov676@gmail.com>
2021-08-13 22:35:21 +0300

    almost done but tests were not found

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -4,7 +4,7 @@ import com.urise.webapp.exeption.ExistStorageException;
 import com.urise.webapp.exeption.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -12,19 +12,18 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class AbstractArrayStorageTest {
-    private static Storage storage;
+    private Storage storage;
 
     private final static String ID_1 = "1";
     private final static String ID_2 = "2";
     private final static String ID_3 = "3";
 
     protected AbstractArrayStorageTest(Storage storage) {
-        AbstractArrayStorageTest.storage = storage;
+        this.storage = storage;
     }
 
-
-    @BeforeAll
-    static void setUp() {
+    @BeforeEach
+    void setUp() {
         storage.clear();
         storage.save(new Resume(ID_1));
         storage.save(new Resume(ID_2));
@@ -39,7 +38,7 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     void updateNotExistElement() {
-        assertThrows(NotExistStorageException.class, ()-> storage.update(new Resume("notExist")));
+        assertThrows(NotExistStorageException.class, () -> storage.update(new Resume("notExist")));
     }
 
     @Test
@@ -51,13 +50,13 @@ public abstract class AbstractArrayStorageTest {
     @Test
     void saveExistElement() {
         storage.save(new Resume("Exist"));
-        assertThrows(ExistStorageException.class, ()->storage.save(new Resume("Exist")));
+        assertThrows(ExistStorageException.class, () -> storage.save(new Resume("Exist")));
     }
 
     @Test
     void saveOverflow() {
 
-   }
+    }
 
     @Test
     void save() {
