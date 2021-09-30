77c712f3e533f4c0f00c9b9a74f71c5ba4db1506
shonmikl <mikhailegorov676@gmail.com>
2021-09-30 17:11:46 +0300

    need to add data

--- src/com/urise/webapp/model/ResumeTestData.java
+++ src/com/urise/webapp/model/ResumeTestData.java
@@ -4,6 +4,11 @@ public class ResumeTestData {
 
     public static void main(String[] args) {
 
+/*
 
+ПРОВЕРЬТЕ свою модель: создайте класс ResumeTestData с методом main, а в нем объект Resume и заполните все его разделы данными, взятыми из Образца резюме. Выведите все секции на консоль
+
+
+ */
     }
 }
