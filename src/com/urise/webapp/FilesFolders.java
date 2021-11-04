f7cc8f143443e31193b40a7089379c2016fab546
shonmikl <mikhailegorov676@gmail.com>
2021-11-04 17:41:00 +0300

    recursive traversal was corrected

--- src/com/urise/webapp/FilesFolders.java
+++ src/com/urise/webapp/FilesFolders.java
@@ -1,26 +1,23 @@
 package com.urise.webapp;
 
 import java.io.File;
+import java.util.Objects;
 
 public class FilesFolders {
 
     public static void main(String[] args) {
-        File path = new File("E:/TopJava/basejava/.");
+        File path = new File("/TopJava/basejava/");
         displayFileName(path);
     }
 
     private static void displayFileName(File path) {
-        if (path.isFile()) {
-            System.out.println(path.getName());
-        } else {
-            System.out.println(path.getName());
-            if (path.listFiles() == null) {
-                System.out.println("File is not exist");
-            } else {
-                File files[] = path.listFiles();
-                for (File dirOrFile : files) {
-                    displayFileName(dirOrFile);
-                }
+        File[] files = path.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            if(files[i].isFile() && files[i] !=null) {
+                System.out.println(Objects.requireNonNull(files[i]).getName());
+            } else if (Objects.requireNonNull(files[i]).isDirectory()) {
+                System.out.println(files[i].getName());
+                displayFileName(files[i]);
             }
         }
     }
