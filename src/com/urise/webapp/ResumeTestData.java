84a8991451fd5a0510a522a81f35a963b412af85
shonmikl <mikhailegorov676@gmail.com>
2021-12-01 17:06:06 +0300

    was corrected

--- src/com/urise/webapp/ResumeTestData.java
+++ src/com/urise/webapp/ResumeTestData.java
@@ -1,6 +1,8 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.*;
+import org.jetbrains.annotations.NotNull;
+
 import java.time.LocalDate;
 import java.util.*;
 
@@ -76,7 +78,7 @@ public class ResumeTestData {
         return resume;
     }
 
-    public static String createsRandomString() {
+    public static @NotNull String createsRandomString() {
         String list = "Q1W2E3R4T5Y6U7I8O9P0A9S8D7F6G5H4J3K2L0M";
         char[] listChar = list.toCharArray();
         Random random = new Random();
